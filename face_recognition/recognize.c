b1aacc5bde54e2055f9e8a8d7fc35e17db6542ea
dfkdream <dfkdream@gmail.com>
2022-12-08 22:05:09 +0900

    typo

--- face_recognition/recognize.c
+++ face_recognition/recognize.c
@@ -18,7 +18,7 @@
 
 extern char** environ;
 
-struct mq_attr attr{
+struct mq_attr attr = {
     .mq_flags = 0,
     .mq_maxmsg = 10,
     .mq_msgsize = BUF_SIZE,
@@ -32,42 +32,38 @@ RECOGNIZE_SET_ON_DONE_CALLBACK cb;
 
 void* msgThread(void* arg){
     char buf[BUF_SIZE];
-    int n;
     while(1){
         memset(buf, 0, sizeof(buf));
-        n = mq_receive(rec_mq_receive, buf, sizeof(buf), NULL);
+        mq_receive(rec_mq_receive, buf, sizeof(buf), NULL);
 
         printf("[MessageWorker] Received: %s\n", buf);
 
         if (strcmp(buf, MSG_RECOGNIZE_OK)){
-            (if cb) cb(1);
+            if (cb) cb(1);
             continue;
         }
 
         if (strcmp(buf, MSG_RECOGNIZE_FAIL)){
-            (if cb) cb(0);
+            if (cb) cb(0);
             continue;
         }
     }
 }
 
-int recognize_init(){
+pid_t recognize_init(){
     rec_mq_send = mq_open(QUEUE_SEND_NAME, O_CREAT|O_WRONLY, 0644, &attr);
-    rec_mq_receive = mq_open(QUEUE_NAME, O_CREAT|O_RDONLY, 0644, &attr);
+    rec_mq_receive = mq_open(QUEUE_RECEIVE_NAME, O_CREAT|O_RDONLY, 0644, &attr);
 
-    if (pthread_create(NULL, NULL, msgThread, NULL)<0){
+    if (pthread_create(NULL, NULL, msgThread, NULL)!=0){
         perror("Could not create message worker thread\n");
         return 0;
     }
 
     pid_t pid;
-    int status;
     char* argv[] = {"python3", "recognize.py", NULL};
 
     posix_spawn(&pid, "python3", NULL, NULL, argv, environ);
-    if (waitpid(pid, &status, 0) < 0) printf("waitpid error\n");
-
-    printf("Face recognizer (%d) exited: %d", pid, status);
+    return pid;
 }
 
 int recognize_start(){
