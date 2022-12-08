d0fd63d96ec4c5910e51e9fee868aedf10972493
dfkdream <dfkdream@gmail.com>
2022-12-09 03:32:33 +0900

    Refactor ultrasonic functions

--- ultrasonic/ultrasonic.c
+++ ultrasonic/ultrasonic.c
@@ -1,49 +1,40 @@
-#include<stdio.h>
-#include<wiringPi.h>
+#include "ultrasonic.h"
+#include <stdio.h>
+#include <pthread.h>
+#include <wiringPi.h>
 
 #define TP 23
 #define EP 24
 
-int main(void)
-{
-	int distance=0;
+pthread_mutex_t lock_ultrasonic;
 
-	long startTime;
-	long travelTime;
+void initUltrasonic(){
+    pthread_mutex_init(&lock_ultrasonic, NULL);
 
-	if(wiringPiSetupGpio () == -1)
-	{
-		printf("Unable GPIO Setup"); 
-		return 1;
-	}
+    if(wiringPiSetupGpio () == -1)
+    {
+        printf("Unable GPIO Setup");
+        return NULL;
+    }
+    pinMode (TP, OUTPUT);
+    pinMode (EP, INPUT);
+}
 
-	pinMode (TP, OUTPUT);
-	pinMode (EP, INPUT);
+int getDistance(){
+    pthread_mutex_lock(&lock_ultrasonic);
 
-	for(;;)
-	{
-		digitalWrite (TP, LOW);
-		delayMicroseconds(2);
-		digitalWrite (TP, HIGH);
-		delayMicroseconds(10);
-		digitalWrite (TP, LOW);
+    digitalWrite (TP, LOW);
+    delayMicroseconds(2);
+    digitalWrite (TP, HIGH);
+    delayMicroseconds(10);
+    digitalWrite (TP, LOW);
 
-		while(digitalRead(EP) == LOW);
+    while(digitalRead(EP) == LOW);
+    startTime = micros();
+    while(digitalRead(EP) == HIGH);
+    travelTime = micros() - startTime;
 
-		startTime = micros();
+    pthread_mutex_unlock(&lock_ultrasonic);
 
-		while(digitalRead(EP) == HIGH);
-		travelTime = micros() - startTime;
-
-		if (travelTime >= 38000){
-		       	printf("out of range\n");
-			delay(200);
-			continue;
-		}
-
-		int distance = travelTime / 58;
-
-		printf( "Distance: %dcm\n", distance);
-		delay(200);
-	}
+    return travelTime / 58;
 }
---
+++ ultrasonic/ultrasonic.h
@@ -0,0 +1,7 @@
+#ifndef SMART_DOORLOCK_ULTRASONIC_H
+#define SMART_DOORLOCK_ULTRASONIC_H
+
+void initUltrasonic();
+int getDistance();
+
+#endif //SMART_DOORLOCK_ULTRASONIC_H
