8cbec6ffa960b30e82014f543213473b07ba3df8
dfkdream <dfkdream@gmail.com>
2022-12-20 15:26:24 +0900

    Fix build script

--- build.sh
+++ build.sh
@@ -4,8 +4,9 @@ mkdir build && cd build
 cmake ..
 make
 cp ../face_recognition/recognize.py .
-read -p "얼굴 사진을 촬영하시겠습니까? (y/n)" $yn
+read -p "얼굴 사진을 촬영하시겠습니까? (y/n)" yn
 case $yn in
     [Yy]* ) raspistill -v -o known.jpg; break;;
     [Nn]* ) exit;;
     * ) exit;;
+esac
